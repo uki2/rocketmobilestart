baadf0b07f452a2a26ee457ed409cd009905b3b3
uki2 <ukitoprosss112@gmail.com>
2024-08-15 22:57:17 +0200

    test na telefonie
    dodalem video czerwony przycisk naprawilem bledy z otwieraniem itd

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,9 +1,33 @@
 extends CanvasLayer
 
+
+#przycisk do debugu lewy gurny rug
+@onready var button_debug := $Button_debug/Buttons_light
+#czerwony przycisk among us
+@onready var button_red_on_rocket = $Button_Red_ON_ROCKET
+#http requesty
 @onready var http := $HTTPRequest
+#animacja pokrywy
+@onready var animation_player := $Klapa3d/SubViewportContainer/SubViewport/AnimationPlayer
+#grren orange red 
+@onready var color_sprawdza = $ColorSprawdza
 
-@onready var rich_text_label = $RichTextLabel
+var timeredstatus := true
+@onready var timer_red := $Timer_red
+@onready var timer_red_fireup := $Timer_red_fireup
 
+#debug visible
+var bvisible: bool = true
+
+var animationfinishedbool := true
+
+var opencase: int = 1
+
+
+var redbuttonpreset := false
+@onready var video_stream_player = $VideoStreamPlayer
+
+#funkcje wlaczenie i wylaczenie leda
 func turn_led_on():
 	# Wysyłanie żądania włączenia diody LED
 	http.request("http://192.168.4.1/?led=on")
@@ -12,15 +36,83 @@ func turn_led_off():
 	# Wysyłanie żądania wyłączenia diody LED
 	http.request("http://192.168.4.1/?led=off")
 
+# 3 state 1 = green 2 = orange itd
+func _process(delta):
+	#print(animationfinishedbool)
+	
+	if redbuttonpreset == true:
+		opencase = 3
+		timer_red.stop()
+	
+	if opencase == 1:
+		color_sprawdza.set_color("green")
+		button_red_on_rocket.disabled = true
+	if opencase == 2:
+		color_sprawdza.set_color("orange")
+		button_red_on_rocket.disabled = true
+	if opencase == 3:
+		color_sprawdza.set_color("red")
+		button_red_on_rocket.disabled = false
+		if timeredstatus == true:
+			timeredstatus = false
+			timer_red.start()
 
 
+
+#button debug left up corner
 func _on_button_on_pressed():
 	turn_led_on()
-
 
 func _on_button_off_pressed():
 	turn_led_off()
 
 
-func _on_http_request_request_completed(result, response_code, headers, body):
-	rich_text_label.text = body.get_string_from_utf8()
+
+
+#mysz weszla
+func _on_klapka_mouse_entered():
+	if animationfinishedbool == true and opencase == 1:
+		animation_player.play("Open")
+		animationfinishedbool = false
+		opencase = 2
+
+func _on_klapka_mouse_exited():
+	if animationfinishedbool == true:
+		pass
+
+
+
+
+# animacje
+func _on_animation_player_animation_finished(anim_name):
+	animationfinishedbool = true
+	if anim_name == ("Close"):
+		opencase = 1
+	if anim_name == ("Open"):
+		opencase = 3
+
+
+func _on_button_debug_pressed():
+	bvisible =! bvisible
+	button_debug.visible = !bvisible
+
+
+func _on_timer_red_timeout():
+	animation_player.play("Close")
+	opencase = 2
+	animationfinishedbool = true
+	timeredstatus = true
+
+#warning!!!!
+func _on_button_red_on_rocket_pressed():
+	#warning!!!!
+	
+	redbuttonpreset = true
+	timer_red_fireup.start()
+	turn_led_on()
+	video_stream_player.play()
+	
+
+
+func _on_timer_red_fireup_timeout():
+	turn_led_off()
